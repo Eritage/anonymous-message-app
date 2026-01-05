9e596155d8dc4a4673839fe9055af2f65f54bb16
Testimony <akinrinolatestimony7@gmail.com>
2026-01-05 15:53:01 +0100

    add features

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 // src/index.ts
 import { Elysia } from "elysia";
+import swagger from "@elysiajs/swagger";
 import { authRoutes } from "./routes/auth";
 import { messageRoutes } from "./routes/messages";
 
 const app = new Elysia()
   .get("/", () => "Anonymous App Backend 🚀")
+  .use(
+    swagger({
+      documentation: {
+        info: {
+          title: "Anonymous Messaging Documentation",
+          version: "1.0.0",
+        },
+      },
+    })
+  )
 
   // Register the routes
   .use(authRoutes)
